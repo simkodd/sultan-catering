f78d112fe5b96fc4b19b103b99f4d3aab2f551a9
unknown <somia.elnahas@gmail.com>
2026-01-19 21:20:17 +0300

    yonlendirme hatasi duzeltildi

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,7 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  async redirects() {
-    return [
-      {
-        source: '/menu', // Eski siteden kalan link
-        destination: '/', // Yeni ana sayfaya gitsin
-        permanent: true,  // Google'a "Artık burası kalıcı" der
-      },
-      {
-        source: '/contact', // Varsa eski iletişim sayfası
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;
